e4b038ae56f524632fff375d81691f995434b2b6
Mohamed Midjek <mohamed.midjek@enigma-school.com>
2021-10-20 15:10:03 +0200

    test spotify

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 let Spotify = require('spotify-web-api-js');
 let s = new Spotify();
+let spotifyApi = new SpotifyWebApi();
+spotifyApi.setAccessToken('<here_your_access_token>');
+
+spotifyApi.getArtistAlbums(
+    '43ZHCT0cAZBISjO8DG9PnE',
+    { limit: 10, offset: 20 },
+    function (err, data) {
+        if (err) console.error(err);
+        else console.log('Artist albums', data);
+    }
+);
