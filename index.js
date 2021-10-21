8d853336daf57c172725a65d733c58fc9dabc8a5
TheoZeisler <theo.zeisler@laposte.net>
2021-10-21 14:33:14 +0200

    feature:web-api

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const rechercheArtiste = () => {
     blocRap.classList.remove("hidden");
     blocRap.classList.add("infos");
 
-    
+
 }
 
 const goHome = () => {
@@ -18,62 +18,101 @@ const goHome = () => {
     blocRap.classList.add("hidden");
     blocRap.classList.remove("infos");
     document.querySelector("#searchText").value = "";
-    parentRap.innerHTML="";
+    parentRap.innerHTML = "";
 }
 
 
 
 const spotifyApi = new SpotifyWebApi();
-spotifyApi.setAccessToken('BQBPh4RQBsMAb4k1MnW5wyqRM2tpO9ZSRUJGlgSb206eATZFFSPThhG4sxOFcEGLlR65mZOezi4Ep6-_1ZoW1Tso2ieAIgNTTIzmkirBKo1YjqkEpdSze6wAztvieDLskA-5g2OKPEBVzl-h4C_a0stDCZVXSbOvZS9q_ZDDcStV');
+spotifyApi.setAccessToken('BQA_0NPdfL0djtLs_klGsRqHYVcwXtEo25uzWzZHFZv6S3hFALvI7hi313R3rCw_uyi3LDfcX-NJFnm5bkwnkIKJcUqa5rlt8Niu04gnhFpoN_FdVIoV05hkgw8ebG1ih-en82oYjUSwe6YbrViy2wtbs5eCggG2v_ZemwmNyUCc');
 
 
-const getArtistAlbums=(id)=>{
-    
-        console.log(id)
+const getArtistAlbums = (id) => {
+
+    console.log(id)
     spotifyApi.getArtistAlbums(
-    id,
-    function (err, data) {
-        console.log(data);
-        parentRap.innerHTML="";
-        data.items.forEach(function (element){
-            console.log(element.name)
-            let ajoutLigne = document.createElement('div');
-            ajoutLigne.textContent = element.name;
-            //ajoutLigne.setAttribute('href',getArtistAlbums(element.id));
-            ajoutLigne.classList.add('line_album');
-            parentRap.append(ajoutLigne);
-            
-          })
-      },
-      function (err) {
-        console.error(err);
-      }
-    );}
+        id,
+        function (err, data) {
+            console.log(data);
+            parentRap.innerHTML = "";
+            data.items.forEach(function (element) {
+                console.log(element.name)
+                let ajoutLigne = document.createElement('div');
+                ajoutLigne.textContent = element.name;
+                //ajoutLigne.setAttribute('href',getArtistAlbums(element.id));
+                ajoutLigne.classList.add('line_album');
+                parentRap.append(ajoutLigne);
+
+            })
+        },
+        function (err) {
+            console.error(err);
+        }
+    );
+}
 
 
 let recherche = document.querySelector("#searchText");
-const chercheArtiste=()=>{
+const chercheArtiste = () => {
     spotifyApi.searchArtists(recherche.value).then(
-    function (data) {
-      console.log(data);
-      parentRap.innerHTML="";
-      data.artists.items.forEach(function (element){
-          console.log(element.name)
-          let ajoutLigne = document.createElement('div');
-          ajoutLigne.textContent = element.name;
-          ajoutLigne.addEventListener("click", ()=>getArtistAlbums(element.id))
-          //ajoutLigne.setAttribute('href',getArtistAlbums(element.id));
-          ajoutLigne.classList.add('line_rapeur');
-          parentRap.append(ajoutLigne);
-          
-        })
-    },
-    function (err) {
-      console.error(err);
-    }
-  );}
+        function (data) {
+            console.log(data);
+            parentRap.innerHTML = "";
+            data.artists.items.forEach(function (element) {
+                console.log(element.name)
+                let ajoutLigne = document.createElement('div');
+                ajoutLigne.textContent = element.name;
+                ajoutLigne.addEventListener("click", () => getArtistAlbums(element.id))
+                //ajoutLigne.setAttribute('href',getArtistAlbums(element.id));
+                ajoutLigne.classList.add('line_rapeur');
+                parentRap.append(ajoutLigne);
+
+            })
+        },
+        function (err) {
+            console.error(err);
+        }
+    );
+}
 
 document.querySelector("#searchText").addEventListener("change", rechercheArtiste);
 document.querySelector("#homeimg").addEventListener("click", goHome);
-recherche.addEventListener("change",chercheArtiste);
+recherche.addEventListener("change", chercheArtiste);
+
+const micBtn = document.querySelector("button");
+const micIcon = micBtn.querySelector("i");
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+
+if (SpeechRecognition) {
+    console.log("marche")
+
+    const recognition = new SpeechRecognition();
+    let mic = "false"
+    micBtn.addEventListener("click", micBtnClick = () => {
+        if (mic == "false") {
+            recognition.start();
+            mic = "true";
+        } else {
+            recognition.stop();
+            mic = "false";
+        }
+    });
+
+    /*recognition.addEventListener("start", startSpeechRecognition = (event) => {
+        console.log(event)
+    })
+    recognition.addEventListener("end", endSpeechRecognition = (event) => {
+        console.log(event)
+    })*/
+    recognition.addEventListener("result", resultOfSpeechRecognition = (event) => {
+        mic = "false";
+        const transcript = event.results[0][0].transcript;
+        recherche.value = transcript + " ";
+        rechercheArtiste();
+        chercheArtiste();
+
+    })
+} else {
+    console.log("marche pas")
+}
 
